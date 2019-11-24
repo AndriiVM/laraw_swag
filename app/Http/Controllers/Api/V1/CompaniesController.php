f4b7fd291306dedd75598db3a37f16b7e19971d2
Andrua <andrey.v.muz@gmail.com>
2019-11-25 00:19:46 +0200

    formas code swagger. Looking for auto test plugin.

--- app/Http/Controllers/Api/V1/CompaniesController.php
+++ app/Http/Controllers/Api/V1/CompaniesController.php
@@ -7,26 +7,12 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 
-/**
- * @OA\Get(
- *     path="/companies",
- *     @OA\Response(response="200", description="An example resource")
- * )
- */
-/**
-  @OA\SecurityScheme(
-      securityScheme="bearerAuth",
-          type="http",
-          scheme="bearer",
-          bearerFormat="JWT"
-      ),
- */
 class CompaniesController extends Controller
 {
 
     /**
      * @OA\GET(
-     *     path="/api/greet",
+     *     path="/greet",
      *     tags={"greeting1"},
      *     summary="Returns a Sample API response",
      *     description="A sample greeting to test out the API",
@@ -40,9 +26,10 @@ class CompaniesController extends Controller
 
     /**
       @OA\Post(
-          path="/v1/login",
+          path="/login",
           tags={"Login1"},
           summary="Login",
+          description="A sample ... to test out the API",
           operationId="login",
 
           @OA\Parameter(
@@ -90,30 +77,31 @@ class CompaniesController extends Controller
 
     /**
      * @OA\Get(
-     * path="/api/v1/companies",
-     * tags={"Companies"},
-     * summary="List of all companies.",
-     * operationId="index",
+     *      path="/companies",
+     *      tags={"Companies"},
+     *      summary="List of all companies.",
+            description="A sample ... to test out the API",
+     *      operationId="index",
      *
-     * @OA\Response(
-     * response=200,
-     * description="Success",
-     * @OA\MediaType(
-     * mediaType="application/json",
-     * )
-     * ),
-     * @OA\Response(
-     * response=401,
-     * description="Unauthorized"
-     * ),
-     * @OA\Response(
-     * response=400,
-     * description="Invalid request"
-     * ),
-     * @OA\Response(
-     * response=404,
-     * description="not found"
-     * ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Success",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized"
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Invalid request"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="not found"
+     *      ),
      * )
      */
     public function index()
@@ -134,11 +122,12 @@ class CompaniesController extends Controller
 
     /**
      * @OA\Post(
-     * path="/api/v1/companies",
-     * tags={"Companies"},
-     * summary="Create a company.",
-     * operationId="store",
-     * @OA\RequestBody(
+     *      path="/companies",
+     *      tags={"Companies"},
+     *      summary="Create a company.",
+            description="A sample ... to test out the API",
+     *      operationId="store",
+     *      @OA\RequestBody(
      *         @OA\MediaType(
      *             mediaType="application/json",
      *             @OA\Schema(
@@ -149,55 +138,55 @@ class CompaniesController extends Controller
      *                 example={"name": "Jessica Smith"}
      *             )
      *         )
-     *     ),
+     *      ),
      *
-     * @OA\Response(
-     * response=201,
-     * description="Success a company creation.",
-     * @OA\MediaType(
-     *     mediaType="application/json",
-     *     @OA\Schema(
-     *                     @OA\Property(
-     *                         property="name",
-     *                         type="string",
-     *                         description="Company name."
-     *                     ),
-     *                     @OA\Property(
-     *                         property="updated_at",
-     *                         type="string",
-     *                         description="Date of update."
-     *                     ),
-     *                     @OA\Property(
-     *                         property="created_at",
-     *                         type="string",
-     *                         description="Date of create.",
-     *                     ),
-     *                     @OA\Property(
-     *                         property="Id",
-     *                         type="integer",
-     *                         description="Record ID.",
-     *                     ),
-     *                     example={
+     *      @OA\Response(
+     *          response=201,
+     *          description="Success a company creation.",
+     *          @OA\MediaType(
+     *              mediaType="application/json",
+     *              @OA\Schema(
+     *                  @OA\Property(
+     *                      property="name",
+     *                      type="string",
+     *                      description="Company name."
+     *                  ),
+     *                  @OA\Property(
+     *                      property="updated_at",
+     *                      type="string",
+     *                      description="Date of update."
+     *                  ),
+     *                  @OA\Property(
+     *                      property="created_at",
+     *                      type="string",
+     *                      description="Date of create.",
+     *                  ),
+     *                  @OA\Property(
+     *                      property="Id",
+     *                      type="integer",
+     *                      description="Record ID.",
+     *                  ),
+     *                  example={
                             "name": "Jessica Smith",
                             "updated_at": "2019-11-21 16:41:42",
                             "created_at": "2019-11-21 16:41:42",
                             "id": 7
-                           }
-     *                 )
-     *     )
-     * ),
-     * @OA\Response(
-     * response=401,
-     * description="Unauthorized"
-     * ),
-     * @OA\Response(
-     * response=400,
-     * description="Invalid request"
-     * ),
-     * @OA\Response(
-     * response=404,
-     * description="not found"
-     * ),
+                        }
+     *              )
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized"
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Invalid request"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="not found"
+     *      ),
      * )
      */
     public function store(Request $request)
@@ -209,11 +198,11 @@ class CompaniesController extends Controller
 
     /**
      * @OA\Get(
-     *      path="/api/v1/companies/{id}",
+     *      path="/companies/{id}",
      *      operationId="show",
      *      tags={"Companies"},
      *      summary="Get company information",
-     *      description="Returns company data",
+     *      description="A sample ... to test out the API",
      *      @OA\Parameter(
      *          name="id",
      *          description="Company id",
@@ -226,56 +215,56 @@ class CompaniesController extends Controller
      *      @OA\Response(
      *          response=200,
      *          description="successful operation",
-                 * @OA\MediaType(
-                 *     mediaType="application/json",
-                 *     @OA\Schema(
-                 *                     @OA\Property(
-                 *                         property="id",
-                 *                         type="integer",
-                 *                         description="Company identifier."
-                 *                     ),
-                 *                     @OA\Property(
-                 *                         property="name",
-                 *                         type="string|null",
-                 *                         description="Company name."
-                 *                     ),
-                 *                     @OA\Property(
-                 *                         property="address",
-                 *                         type="string",
-                 *                         description="Address."
-                 *                     ),
-                 *                     @OA\Property(
-                 *                         property="website",
-                 *                         type="string",
-                 *                         description="Website.",
-                 *                     ),
-                 *                     @OA\Property(
-                 *                         property="email",
-                 *                         type="string",
-                 *                         description="Email.",
-                 *                     ),
-                 *                     @OA\Property(
-                 *                         property="updated_at",
-                 *                         type="string",
-                 *                         description="Date of update."
-                 *                     ),
-                 *                     @OA\Property(
-                 *                         property="created_at",
-                 *                         type="string",
-                 *                         description="Date of create.",
-                 *                     ),
-                 *                     example={
-                                        "id": 1,
-                                        "name": null,
-                                        "address": null,
-                                        "website": null,
-                                        "email": null,
-                                        "created_at": "2019-11-21 15:59:00",
-                                        "updated_at": "2019-11-21 15:59:00"
-                                        }
-                                 )
-                      )
-                  ),
+                @OA\MediaType(
+                     mediaType="application/json",
+                     @OA\Schema(
+                         @OA\Property(
+                              property="id",
+                              type="integer",
+                              description="Company identifier."
+                          ),
+                          @OA\Property(
+                              property="name",
+                              type="string|null",
+                              description="Company name."
+                          ),
+                          @OA\Property(
+                              property="address",
+                              type="string",
+                              description="Address."
+                          ),
+                          @OA\Property(
+                              property="website",
+                              type="string",
+                              description="Website.",
+                          ),
+                          @OA\Property(
+                              property="email",
+                              type="string",
+                              description="Email.",
+                          ),
+                          @OA\Property(
+                              property="updated_at",
+                              type="string",
+                              description="Date of update."
+                          ),
+                          @OA\Property(
+                              property="created_at",
+                              type="string",
+                              description="Date of create.",
+                          ),
+                          example={
+                           "id": 1,
+                           "name": null,
+                           "address": null,
+                           "website": null,
+                           "email": null,
+                           "created_at": "2019-11-21 15:59:00",
+                           "updated_at": "2019-11-21 15:59:00"
+                           }
+     *                  )
+                     )
+                 ),
      *
      *      @OA\Response(response=400, description="Bad request"),
      *      @OA\Response(response=404, description="Resource Not Found"),
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,18 +7,17 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 /**
- * @OA\Info(title="API Laravel VueJS", version="0.1")
- */
-/**
- * @SWG\Swagger(
- *   schemes={"http"},
- *   host="localhost:8000",
- *   basePath="/as",
- *   @SWG\Info(
- *     title="My API",
- *     version="1.0.0"
- *   )
- * )
+ * @OA\Info(
+ *     title="API Laravel 6.5",
+ *     version="0.1",
+ *     description="This is a sample server for ...",
+ *     @OA\Contact(
+ *         name="API Support",
+ *         url="http://www.swagger.io/support",
+ *         email="support@swagger.io"
+ *     )
+ * ),
+ * @OA\Server(url="http://127.0.0.1:8000/api/v1")
  */
 class Controller extends BaseController
 {
